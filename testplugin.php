01cd1f56175947ac3074b4b859bb05ee5390a3ec
PhilippDhn <34065611+PhilippDhn@users.noreply.github.com>
2017-11-29 10:02:28 +0100

    Add files via upload

---
+++ testplugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Unverpackt e.V. Plugin
+Description: Dies ist das Unverpackt e.V. Plugin. Hiermit soll 
+Version: 1.0
+Author: Max Mustermann
+Author URI: http://beispiel.de
+License: GPLv2
+*/
