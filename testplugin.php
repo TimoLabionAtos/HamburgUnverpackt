8c587637f459de6b26ed00017e6fae04a5a6a98b
PhilippDhn <34065611+PhilippDhn@users.noreply.github.com>
2017-11-29 10:55:52 +0100

    Update testplugin.php

--- testplugin.php
+++ testplugin.php
@@ -4,6 +4,5 @@ Plugin Name: Unverpackt e.V. Plugin
 Description: Dies ist das Unverpackt e.V. Plugin. Hiermit sollen laufende Projekte angezeigt werden und User sollen sich für Einsätze anmelden können.
 Version: 1.0
 Author: Matthis Goerdel, Timo Labion, Simon Willen, Philipp Doehrn
-Author URI: http://beispiel.de
-License: GPLv2
+Author URI: https://unverpackt.timo-labion.de
 */
